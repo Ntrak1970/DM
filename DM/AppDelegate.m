e1704d20bcd473a89ec0b7ecc12dd6ad57990830
NTRAK1970 <NTRAK1970@NTRAK1970s-iMac.local>
2014-07-13 11:26:44 -0400

    Initial Commit

---
+++ DM/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  DM
+//
+//  Created by NTRAK1970 on 7/13/14.
+//  Copyright (c) 2014 DM. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ DM/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DM
+//
+//  Created by NTRAK1970 on 7/13/14.
+//  Copyright (c) 2014 DM. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
